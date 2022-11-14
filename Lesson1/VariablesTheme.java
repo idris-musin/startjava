631d488b08f29e2f9ce533a7ce80dc0c2959ef46
idris-musin <musin.idris@gmail.com>
2022-11-14 22:53:50 +0300

    .gitignore adedd

--- Lesson1/VariablesTheme.java
+++ Lesson1/VariablesTheme.java
@@ -25,19 +25,18 @@ public class VariablesTheme {
 
         int penPrice = 100;
         int bookPrice = 200;
-        int diskount = 11;
-        int diskountSum = (penPrice + bookPrice) * 11 / 100;
-        int priceWithDiscount = (penPrice + bookPrice) - diskountSum;
+        int discount = 11;
+        int discountSum = (penPrice + bookPrice) * discount / 100;
+        int dicountPrice = (penPrice + bookPrice) - discountSum;
 
-        System.out.println("Сумма скидки: " + diskountSum);
-        System.out.println("Общая стоимость товара со скидкой: " + priceWithDiscount);
-        
+        System.out.println("Сумма скидки: " + discountSum);
+        System.out.println("Общая стоимость товара со скидкой: " + dicountPrice);
 
         System.out.println("\n3. Вывод слова JAVA");
 
-        System.out.println("   J    a  v     v  a          ");
-        System.out.println("   J   a a  v   v  a a          ");
-        System.out.println("J  J  aaaaa  V V  aaaaa               ");
+        System.out.println("   J    a  v     v  a");
+        System.out.println("   J   a a  v   v  a a");
+        System.out.println("J  J  aaaaa  V V  aaaaa");
         System.out.println(" JJ  a     a  V  a     a");
 
         System.out.println("\n4. Вывод min и max значений целых числовых типов");
@@ -47,28 +46,19 @@ public class VariablesTheme {
         int numInt = 2_147_483_647;
         long numLong = 9_223_372_036_854_775_807L;
 
-        System.out.println(numByte);
-        System.out.println(++numByte);
-        System.out.println(--numByte);
-        System.out.println(numShort);
-        System.out.println(++numShort);
-        System.out.println(--numShort);
-        System.out.println(numInt);
-        System.out.println(++numInt);
-        System.out.println(--numInt);
-        System.out.println(numLong);
-        System.out.println(++numLong);
-        System.out.println(--numLong);
+        System.out.println(numByte + "; " + ++numByte + "; " + --numByte);
+        System.out.println(numShort + "; " + ++numShort + "; " + --numShort);
+        System.out.println(numInt + "; " + ++numInt + "; " + --numInt);
+        System.out.println(numLong + "; " + ++numLong + "; " + --numLong);
 
         System.out.println("\n5. Перестановка значений переменных");
 
         int digit1 = 2;
         int digit2 = 5;
-        int digit3;
+        int digit3 = digit1;
 
         System.out.println("Перестановка с помощью третьей переменной:");
         System.out.println(digit1 + "; " + digit2);
-        digit3 = digit1;
         digit1 = digit2;
         digit2 = digit3;
         System.out.println(digit1 + "; " + digit2);
@@ -93,52 +83,47 @@ public class VariablesTheme {
         char symb94 = '^';
         char symb95 = '_';
 
-        System.out.println((int)symb35 + " " + symb35);
-        System.out.println((int)symb38 + " " + symb38);
-        System.out.println((int)symb64 + " " + symb64);
-        System.out.println((int)symb94 + " " + symb94);
-        System.out.println((int)symb95 + " " + symb95);
+        System.out.println((int) symb35 + " " + symb35);
+        System.out.println((int) symb38 + " " + symb38);
+        System.out.println((int) symb64 + " " + symb64);
+        System.out.println((int) symb94 + " " + symb94);
+        System.out.println((int) symb95 + " " + symb95);
 
         System.out.println("\n7. Вывод количества сотен, десятков и единиц числа");
 
         int num = 123;
 
         int hundreds = num / 100;
-        int dozens = (num % 100) / 10;
-        int units = (num % 10);
+        int tens = (num % 100) / 10;
+        int ones = (num % 10);
 
         System.out.println(num);
         System.out.println(hundreds);
-        System.out.println(dozens);
-        System.out.println(units);
+        System.out.println(tens);
+        System.out.println(ones);
 
         System.out.println("\n8. Вывод в консоль ASCII-арт Дюка");
 
-        char s1 = '/';
-        char s2 = '\\';
-        char s3 = '_';
-        char s4 = '(';
-        char s5 = ')';
+        char slash = '/';
+        char backslash = '\\';
+        char underscore = '_';
+        char parenthesisOpen = '(';
+        char parenthesisClose = ')';
 
-        System.out.println("    " + s1 + s2 + "     ");
-        System.out.println("   " + s1 + "  " + s2 + "    ");
-        System.out.println("  " + s1 + s3 + s4 + " " + s5 + s2 + "   ");
-        System.out.println(" " + s1 + "      " + s2 + "  ");
-        System.out.println("" + s1 + s3 + s3 + s3 + s3 + s1 + s2 + s3 + s3 + s2 + " ");
+        System.out.println("    " + slash + backslash + "     ");
+        System.out.println("   " + slash + "  " + backslash + "    ");
+        System.out.println("  " + slash + underscore + parenthesisOpen + " " + parenthesisClose + backslash + "   ");
+        System.out.println(" " + slash + "      " + backslash + "  ");
+        System.out.println("" + slash + underscore + underscore + underscore + underscore + slash + backslash + 
+            underscore + underscore + backslash + " ");
 
         System.out.println("\n9. Вывод произведения и суммы цифр числа");
 
-        int number = 345;
-
-        int hundredsNum = number / 100;
-        int dozensNum = (number % 100) / 10;
-        int unitsNum = (number % 10);
-
-        int sum = hundredsNum + dozensNum + unitsNum;
-        int multiplication = hundredsNum * dozensNum * unitsNum;
+        int sum = hundreds + tens + ones;
+        int mult = hundreds * tens * ones;
 
         System.out.println("сумма цифр числа N = " + sum);
-        System.out.println("их произведение = " + multiplication);
+        System.out.println("их произведение = " + mult);
 
         System.out.println("\n10. Вывод времени");
 
