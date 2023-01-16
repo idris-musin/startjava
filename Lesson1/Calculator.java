e674f8acc569b6ef9a2471c89f9f6854b60cf50a
idris-musin <musin.idris@gmail.com>
2023-01-16 22:22:49 +0300

    final homework is added

---
+++ Lesson1/Calculator.java
@@ -0,0 +1,27 @@
+public class Calculator {
+    
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+        char sign = '^';
+        int result = 1;
+
+        if (sign == '+') {
+            result = num1 + num2;
+        } else if (sign == '-') {
+            result = num1 - num2;
+        } else if (sign == '*') {
+            result = num1 * num2;
+        } else if (sign == '/') {
+            result = num1 / num2;
+        } else if (sign == '%') {
+            result = num1 % num2;
+        } else if (sign == '^') {
+            for (int i = 1; i <= num2; i++) {
+                result *= num1;
+            }
+        }
+
+        System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+    }
+}
---
+++ Lesson1/GuessNumberGame.java
@@ -0,0 +1,23 @@
+public class GuessNumberGame {
+
+    public static void main(String[] args) {
+        int numFirst = 0;
+        int numLast = 100;
+        int numComp = 89;
+        int numPlayer = 0;
+
+        while (numPlayer != numComp) {
+            numPlayer = numPlayer + (numLast - numFirst) / 2;
+            if (numPlayer > numComp) {
+                System.out.println("Число " + numPlayer + " больше того, что загадал компьютер");
+                numLast = numPlayer;
+                numPlayer = numFirst;
+            } else if (numPlayer < numComp) {
+                System.out.println("Число " + numPlayer + " меньше того, что загадал компьютер");
+                numFirst = numPlayer;
+            }
+        }
+
+        System.out.println("Вы угадали! Загаданное число " + numPlayer);
+    }
+}
