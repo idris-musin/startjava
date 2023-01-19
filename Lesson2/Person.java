e8d7a4771f7f7580056ce9ff74c906cbfd67c788
idris-musin <musin.idris@gmail.com>
2023-01-19 21:17:07 +0300

    homework 2.2 is added

---
+++ Lesson2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String name = "Alexei";
+    String gender = "male";
+    int age = 30;
+    float height = 1.75f;
+    float weight = 70.2f;
+
+    void move() {
+        System.out.println("move");
+    }
+
+    void sit() {
+        System.out.println("sit down");
+    }
+
+    void run() {
+        System.out.println("run");
+    }
+
+    void speak() {
+        System.out.println("talk something");
+    }
+
+    void learnJava() {
+        System.out.println("learn to programming in Java");
+    }
+}
