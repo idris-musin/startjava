d0d2ed499498533d90a679bd2553a443364bf64e
idris-musin <musin.idris@gmail.com>
2023-03-06 22:53:20 +0300

    final homework_4 is added

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,60 +1,51 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
+    private static int num1;
+    private static int num2;
+    private static char operation;
 
-    private int num1;
-    private int num2;
-    private char operation;
-
-    public int getNum1() {
-        return num1;
-    }
-
-    public void setNum1(int num1) {
-        this.num1 = num1;
-    }
-
-    public int getNum2() {
-        return num2;
-    }
-
-    public void setNum2(int num2) {
-        this.num2 = num2;
-    }
-
-    public char getOperation() {
-        return operation;
-    }
-
-    public void setOperation(char operation) {
-        this.operation = operation;
-    }
-
-    public int calculate() {
-        switch (operation) {
-            case '+':
-                return num1 + num2;
-            case '-':
-                return num1 - num2;
-            case '*':
-                return num1 * num2;
-            case '/':
-                if (num2 != 0) {
-                    return num1 / num2;
+    public static double calculate(String str) {
+        parseMathExpression(str);
+        return switch (operation) {
+            case '+' -> (double) Math.addExact(num1, num2);
+            case '-' -> (double) Math.subtractExact(num1, num2);
+            case '*' -> (double) Math.multiplyExact(num1, num2);
+            case '/' -> {
+                if (num2 == 0) {
+                    throw new ArithmeticException("Делить на ноль запрещено!!!");
                 }
-                System.out.println("Делить на ноль нельзя!");
-                return 0;
-            case '%':
-                return num1 % num2;
-            case '^':
-                int result = 1;
-                for (int i = 0; i < num2; i++) {
-                    result *= num1;
+                yield (double) num1 / num2;
+            }
+            case '%' -> {
+                if (num2 == 0) {
+                    throw new ArithmeticException("Делить на ноль запрещено!!!");
                 }
-                return result;
-            default:
-                System.out.println("Вы ввели неверный математический знак!!!");
+                yield (double) num1 % num2;
+            }
+            case '^' -> (int) Math.pow(num1, num2);
+            default -> throw new IllegalStateException("Неверный математический знак: " + operation);
+        };
+    }
+
+    private static void parseMathExpression(String string) {
+        String[] expression = string.split(" ");
+        if (expression.length != 3) {
+            throw new NumberFormatException("Неверный формат математической операции!");
         }
-        return 0;
+        num1 = parseNumber(expression[0]);
+        operation = expression[1].charAt(0);
+        num2 = parseNumber(expression[2]);
+    }
+
+    private static int parseNumber(String string) {
+        double number = Double.parseDouble(string);
+        if (number < 0) {
+            throw new IllegalStateException("Введено отрицательное число!");
+        }
+        if (number % 1 != 0) {
+            throw new IllegalStateException("Введено дробное число!");
+        }
+        return (int) number;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -5,33 +5,32 @@ import java.util.Scanner;
 public class CalculatorTest {
 
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
-
         String answer = "yes";
 
-        while (answer.equals("yes")) {
-            System.out.print("Введите первое число: ");
-            calculator.setNum1(scanner.nextInt());
-            scanner.nextLine();
-            System.out.print("Введите знак математической операции: ");
-            calculator.setOperation(scanner.next().charAt(0));
-            scanner.nextLine();
-            System.out.print("Введите второе число: ");
-            calculator.setNum2(scanner.nextInt());
-            scanner.nextLine();
+        while (!answer.equals("no")) {
+            if (answer.equals("yes")) {
+                double result;
+                System.out.print("Введите математическое выражение: ");
+                String inputExpression = scanner.nextLine();
 
-            System.out.println(calculator.getNum1() + " " +
-                    calculator.getOperation() + " " +
-                    calculator.getNum2() + " = " +
-                    calculator.calculate());
+                try {
+                    System.out.print(inputExpression + " = ");
+                    result = Calculator.calculate(inputExpression);
 
-            do {
-                System.out.println("Хотите продолжить вычисления? [yes/no]");
-                answer = scanner.nextLine();
-            } while (!answer.equals("yes") && !answer.equals("no"));
+                    if (result % 1 == 0) {
+                        System.out.println((int) result);
+                    } else {
+                        System.out.println(result);
+                    }
+                } catch (ArithmeticException | IllegalStateException | NumberFormatException exception) {
+                    System.out.println("Ошибка: " + exception.getMessage());
+                }
+            }
+
+            System.out.println("Хотите продолжить вычисления? [yes/no]");
+            answer = scanner.nextLine();
         }
-
         scanner.close();
     }
 }
